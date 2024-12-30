f63390926a1f831b8f0f7875b102489f35b153bd
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2024-12-30 12:17:05 -0300

    User entity and resource

---
+++ src/main/java/com/acme/project/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.acme.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acme.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Lorenzo", "lorenzobnunez@gmail.com", "(11) 94147-6403", "Oznerol22?");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
